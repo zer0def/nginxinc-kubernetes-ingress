7a2b51a00a642f4b432aaa5b6a1f0a904f04c94e
Lorcan McVeigh <lorcan.mcveigh@gmail.com>
2020-06-10 11:59:49 +0100

    Fix regex error in release scripts

--- hack/prepare-minor-release-in-master.sh
+++ hack/prepare-minor-release-in-master.sh
@@ -18,8 +18,8 @@ fi
 ic_version=$1
 helm_chart_version=$2
 
-prev_ic_version=$(echo $ic_version | awk -F. '{ printf("%s.%s.%d", $1, $2, $3-1) }')
-prev_helm_chart_version=$(echo $helm_chart_version | awk -F. '{ printf("%s.%s.%d", $1, $2, $3-1) }')
+prev_ic_version=$(echo $ic_version | awk -F. '{ printf("%s\\.%s\\.%d", $1, $2, $3-1) }')
+prev_helm_chart_version=$(echo $helm_chart_version | awk -F. '{ printf("%s\\.%s\\.%d", $1, $2, $3-1) }')
 
 sed -i "" "s/$prev_ic_version/$ic_version/g" README.md
 
@@ -30,3 +30,9 @@ sed -i "" -e "s/%%TITLE%%/### $ic_version/g" -e "s/%%IC_VERSION%%/$ic_version/g"
 # update docs CHANGELOG
 sed -i "" "1r hack/changelog-template.txt" docs-web/releases.md 
 sed -i "" -e "s/%%TITLE%%/## NGINX Ingress Controller $ic_version/g" -e "s/%%IC_VERSION%%/$ic_version/g" -e "s/%%HELM_CHART_VERSION%%/$helm_chart_version/g" docs-web/releases.md
+
+# update IC version in the technical-specification doc
+sed -i "" "s/$prev_ic_version/$ic_version/g" docs-web/technical-specifications.md 
+
+# update IC version in the building ingress controller doc
+sed -i "" "s/$prev_ic_version/$ic_version/g" doc-webs/installation/building-ingress-controller-image.md
