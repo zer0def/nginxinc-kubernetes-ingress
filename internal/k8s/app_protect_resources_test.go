8ffdeb3936dce69cdbcff022d22a5d6b63a536be
Rafal Wegrzycki <r.wegrzycki@f5.com>
2020-09-21 18:19:02 +0200

    change variable scope

--- internal/k8s/app_protect_resources_test.go
+++ internal/k8s/app_protect_resources_test.go
@@ -4,15 +4,16 @@ import (
 	"strings"
 	"testing"
 )
-// Positive test cases
-var posDstAntns = []string{"stderr", "syslog:server=localhost:9000", "syslog:server=10.1.1.2:9000", "/var/log/ap.log"}
 
-// Negative test cases item, expected error message
-var negDstAntns = [][]string{{"stdout", "Log Destination did not follow format"},
+func TestValidateAppProtectLogDestinationAnnotation(t *testing.T) {	
+	// Positive test cases
+	var posDstAntns = []string{"stderr", "syslog:server=localhost:9000", "syslog:server=10.1.1.2:9000", "/var/log/ap.log"}
+
+	// Negative test cases item, expected error message
+	var negDstAntns = [][]string{{"stdout", "Log Destination did not follow format"},
 	{"syslog:server=localhost:99999", "not a valid port number"},
 	{"syslog:server=999.99.99.99:5678", "is not a valid ip address"}}
 
-func TestValidateAppProtectLogDestinationAnnotation(t *testing.T) {
 	for _, tCase := range posDstAntns {
 		err := ValidateAppProtectLogDestinationAnnotation(tCase)
 		if err != nil {
