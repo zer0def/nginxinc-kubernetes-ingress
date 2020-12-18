a712b4337c03a8af5b878f495189f712ffb81fe7
Dean Coakley <dean.s.coakley@gmail.com>
2020-12-18 15:06:11 +0000

    Fix NAP test to not create duplicate headers

--- tests/suite/test_app_protect_integration.py
+++ tests/suite/test_app_protect_integration.py
@@ -335,9 +335,9 @@ class TestAppProtect:
 
         print("----------------------- Send valid request ----------------------")
         headers = {
-            "host": ingress_host,
-            "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
-                }
+            "Host": ingress_host,
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
+        }
         response = requests.get(
             appprotect_setup.req_url, headers=headers, verify=False
         )
