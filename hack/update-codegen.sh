070fc8ba7213ad18390d656ec5b6ef9e6827b934
Luca Comellini <luca.com@gmail.com>
2021-01-12 18:44:26 -0800

    Fix path in code-gen script

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -28,5 +28,5 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/nginxinc/kubernetes-ingress/pkg/client github.com/nginxinc/kubernetes-ingress/pkg/apis \
   configuration:v1alpha1,v1 \
-  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
+  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../.." \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
