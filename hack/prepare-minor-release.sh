e190247c8ea3222dcccba1424c9d50f6043eb1fc
Luca Comellini <luca.com@gmail.com>
2021-10-15 19:03:38 -0700

    Update Release description and script (#2091)

--- hack/prepare-minor-release.sh
+++ hack/prepare-minor-release.sh
@@ -10,8 +10,7 @@
 
 DOCS_TO_UPDATE_FOLDER=docs/content
 
-if [ $# != 2 ];
-then
+if [ $# != 2 ]; then
     echo "Invalid number of arguments" 1>&2
     echo "Usage: $0 ic-version helm-chart-version" 1>&2
     exit 1
@@ -29,5 +28,5 @@ prev_helm_chart_version=$(echo $helm_chart_version | awk -F. '{ printf("%s\\.%s\
 hack/common-release-prep.sh $prev_ic_version $ic_version $prev_helm_chart_version $helm_chart_version
 
 # update docs CHANGELOG for minor release
-sed -i "" "9r hack/minor-changelog-template.txt" $DOCS_TO_UPDATE_FOLDER/releases.md
-sed -i "" -e "s/%%TITLE%%/## NGINX Ingress Controller $ic_version/g" -e "s/%%IC_VERSION%%/$ic_version/g" -e "s/%%HELM_CHART_VERSION%%/$helm_chart_version/g"  -e "s/%%DATE%%/$todays_date/g" $DOCS_TO_UPDATE_FOLDER/releases.md
+sed -i "" "8r hack/minor-changelog-template.txt" $DOCS_TO_UPDATE_FOLDER/releases.md
+sed -i "" -e "s/%%TITLE%%/## NGINX Ingress Controller $ic_version/g" -e "s/%%IC_VERSION%%/$ic_version/g" -e "s/%%HELM_CHART_VERSION%%/$helm_chart_version/g" -e "s/%%DATE%%/$todays_date/g" $DOCS_TO_UPDATE_FOLDER/releases.md
