fed389d63c9e62e1dd9ff532e190feb8908a6c82
Ciara Stacke <c.stacke@f5.com>
2022-01-11 13:26:37 +0000

    Update JWT in tests and add flaky reruns to common failures

--- tests/suite/test_virtual_server_grpc.py
+++ tests/suite/test_virtual_server_grpc.py
@@ -134,6 +134,7 @@ class TestVirtualServerGrpc:
                 print(e.details())
                 pytest.fail("RPC error was not expected during call, exiting...")
 
+    @pytest.mark.flaky(max_runs=3)
     @pytest.mark.parametrize("backend_setup", [{"app_type": "grpc-vs"}], indirect=True)
     def test_grpc_error_intercept(self, kube_apis, ingress_controller_prerequisites, crd_ingress_controller, 
                                   backend_setup, virtual_server_setup):
