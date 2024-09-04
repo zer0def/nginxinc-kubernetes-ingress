90a52a002a2d04c7590b5d17e06b3d38ed6f28b3
Paul Abel <128620221+pdabelf5@users.noreply.github.com>
2024-09-04 17:35:32 +0100

    move mounted code to fix /tmp file permissions (#6354)

--- build/scripts/common.sh
+++ build/scripts/common.sh
@@ -5,20 +5,20 @@ set -e
 PLUS=""
 if [ -z "${BUILD_OS##*plus*}" ]; then
     mkdir -p /etc/nginx/oidc/
-    cp -a /tmp/internal/configs/oidc/* /etc/nginx/oidc/
+    cp -a /code/internal/configs/oidc/* /etc/nginx/oidc/
     mkdir -p /etc/nginx/state_files/
     PLUS=-plus
 fi
 
-mkdir -p /etc/nginx/njs/ && cp -a /tmp/internal/configs/njs/* /etc/nginx/njs/
+mkdir -p /etc/nginx/njs/ && cp -a /code/internal/configs/njs/* /etc/nginx/njs/
 mkdir -p /var/lib/nginx /etc/nginx/secrets /etc/nginx/stream-conf.d
 setcap 'cap_net_bind_service=+eip' /usr/sbin/nginx 'cap_net_bind_service=+eip' /usr/sbin/nginx-debug
 setcap -v 'cap_net_bind_service=+eip' /usr/sbin/nginx 'cap_net_bind_service=+eip' /usr/sbin/nginx-debug
 
-cp -a /tmp/internal/configs/version1/nginx$PLUS.ingress.tmpl \
-    /tmp/internal/configs/version1/nginx$PLUS.tmpl \
-	/tmp/internal/configs/version2/nginx$PLUS.virtualserver.tmpl \
-    /tmp/internal/configs/version2/nginx$PLUS.transportserver.tmpl \
+cp -a /code/internal/configs/version1/nginx$PLUS.ingress.tmpl \
+    /code/internal/configs/version1/nginx$PLUS.tmpl \
+	/code/internal/configs/version2/nginx$PLUS.virtualserver.tmpl \
+    /code/internal/configs/version2/nginx$PLUS.transportserver.tmpl \
     /
 
 chown -R 101:0 /etc/nginx /var/cache/nginx /var/lib/nginx /var/log/nginx /*.tmpl
