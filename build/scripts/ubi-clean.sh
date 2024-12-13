90c2e8ab9c0b7e65b0ccf2d99eaf84ff151838e8
Paul Abel <128620221+pdabelf5@users.noreply.github.com>
2024-12-13 17:12:20 +0000

    remove packages not required (#6989)

--- build/scripts/ubi-clean.sh
+++ build/scripts/ubi-clean.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-microdnf remove -y shadow-utils subscription-manager
+microdnf remove -y shadow-utils subscription-manager python3-requests python3-cloud-what python3-subscription-manager-rhsm python3-setuptools python3-inotify python3-requests python3-urllib3 python3-idna
 microdnf clean all && rm -rf /var/cache/dnf
