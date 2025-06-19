c45c04956d337840bc9e521b9b6625b9ac165692
Gabor Javorszky <g.javorszky@f5.com>
2025-06-19 17:31:11 +0100

    Pin mvdan/gofumpt in github workflow ci and fix bash variable quoting (#7923)
    * Pin mvdan/gofumpt to v0.8.0 instead of latest
    
    * Quote bash variables to prevent globbing
    
    See shellcheck SC2053
    https://github.com/koalaman/shellcheck/wiki/SC2053

--- hack/docker.sh
+++ hack/docker.sh
@@ -4,12 +4,12 @@ git_tag=$1
 docker_tag=edge
 
 git_commit=$(git rev-parse HEAD)
-commit_tag=$(git describe --exact-match ${git_commit} 2>/dev/null)
+commit_tag=$(git describe --exact-match "${git_commit}" 2>/dev/null)
 
-if [[ ${commit_tag} == ${git_tag} ]]; then
+if [[ ${commit_tag} == "${git_tag}" ]]; then
     # we're on the exact commit of the tag, use the docker image for the tag
     docker_tag=${git_tag//v/}
-    echo ${docker_tag}
+    echo "${docker_tag}"
     exit 0
 
 else
@@ -17,7 +17,7 @@ else
     # if it's the latest commit from 'main' the SHA will match the 'revision' of the 'edge' docker image
     docker pull nginx/nginx-ingress:${docker_tag} >/dev/null 2>&1
     DOCKER_SHA=$(docker inspect --format '{{ index .Config.Labels "org.opencontainers.image.revision" }}' nginx/nginx-ingress:${docker_tag})
-    if [[ ${DOCKER_SHA} == ${git_commit} ]]; then
+    if [[ ${DOCKER_SHA} == "${git_commit}" ]]; then
         # we're on the same commit as the latest edge
         echo ${docker_tag}
         exit 0
